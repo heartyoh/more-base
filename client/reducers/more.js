d2e2ed9645314b5fe01cddde1c13a2850fcf402e
Hearty Oh <heartyoh@hatiolab.com>
2019-07-18 20:38:34 +0900

    v0.1.3

--- client/reducers/more.js
+++ client/reducers/more.js
@@ -1,24 +1,11 @@
 import { ADD_MORENDA } from '../actions/more'
 
 const INITIAL_STATE = {
-  show: false,
   morendas: []
 }
 
 const more = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case 'OPEN_MORE_PANEL':
-      return {
-        ...state,
-        show: true
-      }
-
-    case 'CLOSE_MORE_PANEL':
-      return {
-        ...state,
-        show: false
-      }
-
     case ADD_MORENDA:
       let morenda = action.morenda
       return {
